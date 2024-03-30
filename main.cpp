4146381eeeabfada26df9738413098cdedfce221
Emmett-Kogan <emmett.kogan@gmail.com>
2024-03-30 14:30:49 -0400

    Finished decompression and am passing the decompression test case. Need to just finish up compression and generating cout.txt and I'm done :)

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@
 #include <set>
 #include <string>
 
+#include <iostream>
+
 #define INPUT_C "original.txt"
 #define OUTPUT_C "cout.txt"
 #define INPUT_D "compressed.txt"
@@ -19,23 +21,18 @@
 
 #define ERREXIT(s) { fprintf(stderr, s); exit((-1)); }
 
-using std::unordered_map, std::vector, std::set, std::string;
+using std::unordered_map, std::vector, std::set, std::string, std::stoi;
+using namespace std;
 
-void compress(FILE *fptr_in, FILE *fptr_out);
-void decompress(FILE *fptr_in, FILE *fptr_out);
+void compress(FILE *const fptr_in, FILE *const fptr_out);
+void decompress(FILE *const fptr_in, FILE *const fptr_out);
 uint32_t btoi(const char *const str, uint32_t len);
 
-// I think it would be more helpful to have compress and decompress
-// act as their own programs pretty much. For compress, it makes sense to
-// treat each line individually and rebuild from there
-// but for decompress, it would probably be better to just make a parser that
-// runs on a really long binary string
-// so instead of parseing them in main, just opening the correct files and calling
-// compress/decompress to actually parse the input file might be better
-// this would also prevent me from needing global variables to do this
+// Note that this does not include the 3 bit format identifier, it's just the rest of the format
+uint32_t FORMAT_LENGTH[8] = {32, 3, 13, 9, 9, 9, 14, 4};
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
     if (argc < 2)
         ERREXIT("Missing cmd line arg\n");
 
@@ -58,7 +55,8 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void compress(FILE *fp_in, FILE *fp_out) {
+void compress(FILE *const fp_in, FILE *const fp_out) 
+{
     vector<uint32_t> lines;
     unordered_map<uint32_t, uint32_t> counts;
 
@@ -111,35 +109,151 @@ void compress(FILE *fp_in, FILE *fp_out) {
     }
 
     // Now the actual compression
+    for (auto line : lines) {
+
+    }
+    // What this will probably look like:
+        // In order of which one compresses most
+            // Check if we can compress like this
+                // If we can, compress using that method
+                // note for RLE I might have to mess with which line im on, and count ahead
+        // Add compressed text to large string
+        // repeat until out of lines
+
+        // When checking for the bit differences, may be best to search through dictionary
+        // xor the dictionary values with the line being compressed and count bits that are high
+        // select the compression with the minimum overhead that is compatible
+
+
+    // For now I think I want this to just append to a large string like in decompress
+    // And then later I can write that to a file so that I have newlines all in the correct places
+
+    // Something like this should work, where bstr is the big string
+    // string bstr;
+    // for (unsigned long i = 0; i < bstr.length(); i++) {
+    //     if (i % 32 == 0 && i)
+    //         fputc('\n', fp_out);
+    //     fputc(bstr[i], fp_out);
+    // }
+    // fputs("xxxx\n", fp_out);
+    // for (int i = 0; i < 16; i++)
+    //     fprintf(fp_out, "%.032b\n", dictionary[i]);
+
 }
 
-void decompress(FILE *fp_in, FILE *fp_out) {
+void decompress(FILE *const fp_in, FILE *const fp_out) 
+{
     string bstr;
     uint32_t dictionary[16];
 
-    // Goal is to read in the file as a bigass binary string
+    // Read just compressed text data into one string
     char buffer[34];
     while (fgets(buffer, 34, fp_in)) {
+        #ifdef DEBUG
         printf("%s", buffer);
+        #endif
+        
         if (!strncmp(buffer, "xxxx", 4))
             break;
         for (int i = 0; i < 34; i++)
             if (buffer[i] == '0' || buffer[i] == '1')
                 bstr += buffer[i];
     }
-    bstr += '\0';
 
-    // At this point, we have read the line with "xxxx" and
-    // the next line we read will be the first line of the dictionary
-    // Then load the dictionary
+    // Load dictionary
     for (int i = 0; i < 16; i++) {
         fgets(buffer, 34, fp_in);
         dictionary[i] = btoi(buffer, 32);
+
+        #ifdef DEBUG
         printf("%.032b\n", dictionary[i]);
+        #endif
     }
 
+    if (fgets(buffer, 34, fp_in))
+        ERREXIT("Should have reached EOF but didn't");
+
+    // Note that when there are offsets for bitshifting, technically underflow is possible
+    // But since the values that would cause underflow don't make sense, e.g. in 4 bit mismatch case
+    // it doesn't make sense for the start bit to be the LSB, I'm not going to boundscheck
 
     // Then parse the bigass string and decompress
+    uint32_t prev_instruction = 0, offset = 0, length = bstr.length();
+    while (offset < length) {
+
+        #ifdef DEBUG
+        printf("Offset:   %d\n", offset);
+        #endif
+
+        // Get 3 bits starting at index
+        uint8_t format = stoi(bstr.substr(offset, 3), 0, 2), print_flag = 1;
+        offset += 3; // move index to start of rest of encoding
+
+        #ifdef DEBUG
+        printf("Format:   %d\n", format);
+        printf("Encoding: %s\n", bstr.substr(offset,FORMAT_LENGTH[format]).c_str());
+        #endif
+
+        switch(format) {
+        case 0:
+            if (length - offset < 32) {
+                print_flag = 0;
+                break;
+            }
+
+            prev_instruction = stoul(bstr.substr(offset,32).c_str(), 0, 2);
+            break;
+        case 1:
+        {
+            // next three bits as an int, +1, and print prev_instruction that many times
+            int repeat = stoi(bstr.substr(offset, 3).c_str(), 0, 2)+1;
+            printf("Repeat: %d\n", repeat);
+            for (int i = 0; i < repeat; i++)
+                fprintf(fp_out, "%.032b\n", prev_instruction);
+            print_flag = 0;
+            break;
+        }
+        case 2:
+        {
+            uint32_t start = stoi(bstr.substr(offset, 5).c_str(), 0, 2);
+            uint32_t bm = stoi(bstr.substr(offset+5, 4).c_str(), 0, 2);
+            uint32_t dict_val = dictionary[stoi(bstr.substr(offset+9,4).c_str(), 0, 2)];
+            prev_instruction = dict_val^(bm<<(32-start-4));
+            break;
+        }
+        case 3:
+        case 4:
+        case 5:
+        {
+            // Nested ternary making sense lol
+            uint32_t bm = (format == 3 ? 0b1 : (format == 4 ? 0b11 : 0b1111));
+            uint32_t width = (format == 3 ? 1 : (format == 4 ? 2 : 4));
+            uint32_t start = stoi(bstr.substr(offset, 5).c_str(), 0, 2);
+            uint32_t dict_val = dictionary[stoi(bstr.substr(offset+5, 4).c_str(), 0, 2)];
+            prev_instruction = dict_val^(bm<<(32-start-width));
+            break;
+        }
+        case 6:
+        {
+            uint32_t ml1 = stoi(bstr.substr(offset, 5).c_str(), 0, 2);
+            uint32_t ml2 = stoi(bstr.substr(offset+5, 5).c_str(), 0, 2);
+            uint32_t dict_val = dictionary[stoi(bstr.substr(offset+10,4).c_str(), 0, 2)];
+            prev_instruction = dict_val^(0b1<<(32-ml1-1));
+            prev_instruction ^= (0b1<<(32-ml2-1));
+            break;
+        }
+        case 7:
+            prev_instruction = dictionary[stoi(bstr.substr(offset,4).c_str(), 0, 2)];
+            break;
+        default:
+            ERREXIT("Something went very very wrong");
+        }
+
+        if (print_flag)
+            fprintf(fp_out, "%.032b\n", prev_instruction);
+
+        offset += FORMAT_LENGTH[format];
+    }
 }
 
 uint32_t btoi(const char *const str, uint32_t len) {
